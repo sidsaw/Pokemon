dd8008e643a94c2d591e9cb050501063ce4b13ec
Sidharth Sawhney <sidsaw@umich.edu>
2020-07-04 16:36:46 -0400

    decode

--- qplserver.py
+++ qplserver.py
@@ -35,7 +35,7 @@ def server():
 			# TODO Update database with stats for new files
 			print("Printing files")
 			for file in files:
-				print(files)
+				print(file.decode("utf-8"))
 			logstring = 'Updated replays with fileids: ' + ' '.join(parsedids) + ' ignored replayed with fileids: ' + ' '.join(droppedids)
 			data = {
 				'log': logstring
