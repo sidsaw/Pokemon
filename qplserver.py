c12eeaeab995a9bc15c09327bc458afc6c32aae4
Sidharth Sawhney <sidsaw@umich.edu>
2020-07-04 16:13:31 -0400

    added print statements

--- qplserver.py
+++ qplserver.py
@@ -8,28 +8,52 @@ print("Starting Pokemon Server!")
 @app.route('/', methods=['GET', 'POST'])
 def server():
 	if request.method == 'POST':
+		print("detected post request")
 		requestdata = request.get_json()
 		# if fileids is in data
 		if requestdata.get('fileids') != None:
+			print("detected fileids in form data")
 			fileids = requestdata.get('fileids')
-			print('fileids')
-			print(fileids)
+			# TODO query database and find which file ids needed
 			data = {
-				'message': 'Received post request and fileids',
+				'log': 'Received post request and fileids',
 				'idsneeded': fileids
 			}
 			return jsonify(data)
 
+		# if files and neededids are in data
+		else if (requestdata.get('files') != None and
+				requestdata.get('idsneeded') != None):
+			print("detected files and idsneeded in form data")
+			files = requestdata.get('files')
+			idsneeded = requestdata.get('idsneeded')
+			# TODO Parse files for replayid
+			# TODO check if replay was already parsed
+			droppedids = []
+			# TODO change statement below
+			parsedids = idsneeded
+			# TODO Update database with stats for new files
+			print("Printing files")
+			for file in files:
+				print(files)
+			logstring = 'Updated replays with fileids: ' + parsedids + ' ignored replayed with fileids: ' + droppedids
+			data = {
+				'log': logstring
+			}
+			return jsonify(data)
+
 		else:
+			print("did not detect fileids, files, idsneeded in form data")
 			# TODO change to sending back a status code as well
 			data = {
-				'message': 'Received post request but did not find fileids'
+				'log': 'Received post request but did not find fileids or (files and idsneeded)'
 			}
 			return jsonify(data)
 
 	# TODO change to sending back a status code as well
+	print("did not detect post request")
 	data = {
-		'message': 'Did not receive post request'
+		'log': 'Did not receive post request'
 	}
 	return jsonify(data)
 
