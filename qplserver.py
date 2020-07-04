0ae87ebc88af6298db9961c2176afc681f577ac9
Sidharth Sawhney <sidsaw@umich.edu>
2020-07-04 13:56:52 -0400

    checks for json

--- qplserver.py
+++ qplserver.py
@@ -1,13 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
 app = Flask(__name__)
 
 print("Starting Pokemon Server!")
 
 # Listen for post requests
 @app.route('/', methods=['GET', 'POST'])
-def result():
-	print("Received packets")
-	return 'Received!'
+def server():
+	if request.method == 'POST':
+		# if fileids is in data
+		if request.args.get('fileids') != None:
+			fileids = request.args.get('fileids')
+			data = {'message': 'Received post request and fileids'}
+			return jsonify(data)
+
+		else:
+			data = {'message': 'Received post request but did not find fileids'}
+			return jsonify(data)
+
+	data = {'message': 'Did not receive post request'}
+	return jsonify(data)
 
 
 # Get file list
